983ec6ddb8220d97dc9622bf268076324fd6f081
Marc Gauch <34353267+marcgauch@users.noreply.github.com>
2023-04-14 16:09:34 +0200

    CHANGED: removed all default code but added element-plus

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//import 'element-plus/theme-chalk/dark/css-vars.css'
+
+import '@/styles/index.scss'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
