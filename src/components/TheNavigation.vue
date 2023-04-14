c50929fe6d4273972009657905bd8dd4acd7cbab
marcgauch <34353267+marcgauch@users.noreply.github.com>
2023-04-14 21:34:37 +0200

    ADDED: Create Dataset

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -7,20 +7,13 @@
     active-text-color="#ffd04b"
     router
   >
-    <el-menu-item index="1"></el-menu-item>
     <el-sub-menu index="2">
-      <template #title>Workspace</template>
-      <el-menu-item index="/">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu>
+      <template #title>Create Dataset</template>
+      <el-menu-item index="/create/windows">Windows</el-menu-item>
+      <el-menu-item index="/create/mac">Mac</el-menu-item>
+      <el-menu-item index="/create/linux">Linux</el-menu-item>
     </el-sub-menu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">Orders</el-menu-item>
+    <el-menu-item index="/upload">Upload</el-menu-item>
+    <el-menu-item index="/view">View</el-menu-item>
   </el-menu>
 </template>
---
+++ src/views/CreateView.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-tabs type="border-card" v-model="selectedTab">
+    <el-tab-pane label="Windows" name="windows"><el-text tag="i">todo</el-text></el-tab-pane>
+    <el-tab-pane label="Mac" name="mac"><el-text tag="i">todo</el-text></el-tab-pane>
+    <el-tab-pane label="Linux" name="linux">
+      <el-text tag="i">tree -a -J -R -s ~/ownCloud/ > ~/tree.json</el-text>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const selectedTab = ref(route.params.os.toString());
+
+watch(
+  () => route.params.os,
+  (newOs) => {
+    if (typeof newOs === 'string') {
+      selectedTab.value = newOs;
+    } else {
+      selectedTab.value = newOs[0];
+    }
+  }
+);
+</script>
