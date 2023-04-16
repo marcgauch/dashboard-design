c06c40366411a71f8003b11e05a34643aadd7b81
marcgauch <34353267+marcgauch@users.noreply.github.com>
2023-04-16 19:54:30 +0200

    ADDED: Option to save data in storage

--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -10,14 +10,33 @@
       </div>
     </template>
   </el-upload>
+  <el-divider />
+  <el-card>
+    <el-checkbox v-model="settingsStore.DEBUG_SAVE_UPLOADED_DATA_IN_LOCAL_STORAGE">
+      DEBUG: Save and restore uploaded data automatically
+    </el-checkbox>
+    <br />
+    <el-button @click="settingsStore.saveSettings()">Save Settings</el-button>
+    <el-button @click="settingsStore.loadSettings()">Load Settings</el-button>
+    <br /><br />
+    <el-button @click="loadDatasetFromStorage">Load Dataset from storage</el-button>
+  </el-card>
 </template>
 
 <script setup lang="ts">
 import { UploadFilled } from '@element-plus/icons-vue';
 import { ReportFileParser } from '@/services/ReportFileParser';
 import { useTreeStore } from '@/stores/treeStore';
+import { useSettingsStore } from '@/stores/settingsStore';
 
 const treeStore = useTreeStore();
+const settingsStore = useSettingsStore();
+
+const loadDatasetFromStorage = () => {
+  const DATA = localStorage.getItem('DATA');
+  if (!DATA) return;
+  treeStore.reportFile = ReportFileParser.parse(DATA, 'from storage');
+};
 
 const upload = (file: any) => {
   //console.log(file);
@@ -27,8 +46,16 @@ const upload = (file: any) => {
     const str = e?.target?.result?.toString();
     if (!str) return;
     treeStore.reportFile = ReportFileParser.parse(str, name);
+    if (settingsStore.DEBUG_SAVE_UPLOADED_DATA_IN_LOCAL_STORAGE) {
+      localStorage.setItem('DATA', str);
+    }
   };
 
   FR.readAsText(raw);
 };
+
+if (settingsStore.DEBUG_SAVE_UPLOADED_DATA_IN_LOCAL_STORAGE) {
+  settingsStore.loadSettings();
+  loadDatasetFromStorage();
+}
 </script>
