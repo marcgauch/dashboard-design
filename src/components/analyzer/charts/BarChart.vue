45628023f1bc7806c1466b084fc957137e6a2b3f
marcgauch <34353267+marcgauch@users.noreply.github.com>
2023-04-16 16:17:10 +0200

    ADDED: First Diagram

--- src/components/analyzer/charts/BarChart.vue
+++ src/components/analyzer/charts/BarChart.vue
@@ -1,8 +1,14 @@
 <template>
-  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+  <Bar v-if="showChart" id="my-chart-id" :options="chartOptions" :data="chartData" />
+  <pre>
+    {{ chartOptions }}
+  </pre>
 </template>
 
 <script setup lang="ts">
+import { nextTick, reactive, ref } from 'vue';
+import { UTIL } from '@/services/Util';
+import { useAnalyzeStore } from '@/stores/analyzeStore';
 import { Bar } from 'vue-chartjs';
 import {
   Chart as ChartJS,
@@ -13,14 +19,49 @@ import {
   CategoryScale,
   LinearScale,
 } from 'chart.js';
-import { reactive } from 'vue';
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+const analyzeStore = useAnalyzeStore();
+analyzeStore.$subscribe(async () => {
+  const largestFiles = analyzeStore.filesSortedBySize.slice(0, 10);
+  let labels = [] as string[];
+  let data = [] as number[];
+  largestFiles.forEach((f) => {
+    labels.push(f.name);
+    data.push(f.size);
+  });
+  showChart.value = false;
+  chartData.labels = labels;
+  chartData.datasets[0].data = data;
+  await nextTick();
+  showChart.value = true;
+});
 
 const chartData = reactive({
   labels: ['January', 'February', 'March'],
   datasets: [{ data: [40, 20, 12] }],
 });
 
-const chartOptions = reactive({ responsive: true });
+const chartOptions = reactive({
+  responsive: true,
+  maintainAspectRatios: false,
+  plugins: {
+    title: {
+      display: true,
+      text: 'Largest Files',
+    },
+    tooltip: {
+      callbacks: {
+        label: (tooltipItems: any) => {
+          return [
+            UTIL.calculateSize(tooltipItems.raw),
+            analyzeStore.filesSortedBySize[tooltipItems.datasetIndex].fullPath,
+          ];
+        },
+      },
+    },
+  },
+});
+const showChart = ref(false);
 </script>
