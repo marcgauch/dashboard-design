ee537c3d35df8286e2c485e2f5bc79bf8dc1493b
marcgauch <34353267+marcgauch@users.noreply.github.com>
2023-05-21 14:57:10 +0200

    ADDED: Breadcrumbs in title

--- src/components/analyzer/titleWithFilter/TheTitle.vue
+++ src/components/analyzer/titleWithFilter/TheTitle.vue
@@ -1,7 +1,7 @@
 <template>
   <el-card body-style="padding-top: 0.8rem; padding-bottom: 0.8rem;">
     <template #header>
-      You are watching <el-text tag="b">{{ analyzeStore.directoryPath }}</el-text> in detail
+      <TitleBreadcrumb />
     </template>
     <TheFilter />
   </el-card>
@@ -9,7 +9,5 @@
 
 <script setup lang="ts">
 import TheFilter from './TheFilter.vue';
-import { useAnalyzeStore } from '@/stores/analyzeStore';
-
-const analyzeStore = useAnalyzeStore();
+import TitleBreadcrumb from './TitleBreadcrumb.vue';
 </script>
---
+++ src/components/analyzer/titleWithFilter/TitleBreadcrumb.vue
@@ -0,0 +1,55 @@
+<template>
+  <div style="display: flex; flex-wrap: wrap; flex-basis: content">
+    <div>You are watching&nbsp;</div>
+    <el-tooltip placement="top" content="Set as analyze directory">
+      <div class="link-item" @click="analyzeRoot">{{ root }}</div>
+    </el-tooltip>
+    <template v-for="(folder, i) in folders">
+      <div>/</div>
+      <el-tooltip placement="top" content="Set as analyze directory">
+        <div class="link-item" @click="analyzeFolder(i)">{{ folder }}</div>
+      </el-tooltip>
+    </template>
+    <div>&nbsp;in detail</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAnalyzeStore } from '@/stores/analyzeStore';
+import { useTreeStore } from '@/stores/treeStore';
+import { computed } from 'vue';
+
+const analyzeStore = useAnalyzeStore();
+const treeStore = useTreeStore();
+
+const root = computed(() => treeStore.root.fullPath.replace(/\/+$/, ''));
+
+const folders = computed(() =>
+  analyzeStore.directoryPath.replace(treeStore.root.fullPath, '').replace(/\/+$/, '').split('/')
+);
+
+const analyzeRoot = () => {
+  analyzeStore.setDirectory(treeStore.root);
+};
+
+const analyzeFolder = (index: number) => {
+  // find directory by parent
+  const stepsToGo = folders.value.length - index - 1;
+  let directory = analyzeStore.analyzeDirectory;
+  for (let i = 0; i < stepsToGo; i++) {
+    directory = directory.parent!;
+  }
+
+  analyzeStore.setDirectory(directory);
+};
+</script>
+
+<style scoped>
+.link-item {
+  color: var(--el-color-primary);
+  cursor: pointer;
+}
+.link-item:hover {
+  color: var(--el-color-primary-light-5);
+}
+</style>
