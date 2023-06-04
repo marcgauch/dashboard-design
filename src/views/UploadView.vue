24bd291ecc5099b817356fecf2137f34f06e1696
marcgauch <34353267+marcgauch@users.noreply.github.com>
2023-06-04 11:23:48 +0200

    REMOVED: Debug stuff

--- src/views/UploadView.vue
+++ src/views/UploadView.vue
@@ -17,40 +17,21 @@
     close-text="Gotcha"
     style="margin-top: 1rem"
   />
-  <el-divider />
-  <el-card>
-    Or use our <el-button @click="useDemoData" type="primary">Demo Data</el-button><br />
-    <el-checkbox v-model="settingsStore.DEBUG_SAVE_UPLOADED_DATA_IN_LOCAL_STORAGE">
-      DEBUG: Save and restore uploaded data automatically
-    </el-checkbox>
-    <br />
-    <el-button @click="settingsStore.saveSettings()">Save Settings</el-button>
-    <el-button @click="settingsStore.loadSettings()">Load Settings</el-button>
-    <br /><br />
-    <el-button @click="loadDatasetFromStorage">Load Dataset from storage</el-button>
-  </el-card>
+  Or use our <el-button @click="useDemoData" type="primary">Demo Data</el-button>
 </template>
 
 <script setup lang="ts">
 import { UploadFilled } from '@element-plus/icons-vue';
 import { ReportFileParser } from '@/services/ReportFileParser';
 import { useTreeStore } from '@/stores/treeStore';
-import { useSettingsStore } from '@/stores/settingsStore';
 import { ElNotification } from 'element-plus';
 import DEMODATA from '@/services/demodata';
 
 const treeStore = useTreeStore();
-const settingsStore = useSettingsStore();
 
 const useDemoData = () => {
   treeStore.reportFile = ReportFileParser.parse(JSON.stringify(DEMODATA), 'Demodata.json');
   showUploadedNotification();
-};
-
-const loadDatasetFromStorage = () => {
-  const DATA = localStorage.getItem('DATA');
-  if (!DATA) return;
-  treeStore.reportFile = ReportFileParser.parse(DATA, 'from storage');
 };
 
 const upload = (file: any) => {
@@ -60,13 +41,6 @@ const upload = (file: any) => {
     const str = e?.target?.result?.toString();
     if (!str) return;
     treeStore.reportFile = ReportFileParser.parse(str, name);
-    if (settingsStore.DEBUG_SAVE_UPLOADED_DATA_IN_LOCAL_STORAGE) {
-      try {
-        localStorage.setItem('DATA', str);
-      } catch {
-        console.log("Can't add your data to store. Probably it's just too big");
-      }
-    }
     showUploadedNotification();
   };
 
@@ -80,9 +54,4 @@ const showUploadedNotification = () => {
     type: 'success',
   });
 };
-
-if (settingsStore.DEBUG_SAVE_UPLOADED_DATA_IN_LOCAL_STORAGE) {
-  settingsStore.loadSettings();
-  loadDatasetFromStorage();
-}
 </script>
